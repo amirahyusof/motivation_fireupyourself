ba676195e076c7f8afae858f80e64e78475067dc
amirah <mirez.data@gmail.com>
2023-12-15 22:55:29 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import "./index.css"
 
 function App() {
   return (
-    <div className='square-section'>
-      <h1>Hello World</h1>
-
+    <div className="square-section">
+      <div className="content-section">
+      <h3 className="intro-part">Hey, I need motivation</h3>
+      <h4 className="motiv-part">Motivation by me</h4>
+      <button>FireUp 🔥 </button>
+      </div>
     </div>
   );
 }
