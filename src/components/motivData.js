e4362387f68c6c9c7fc92102059a47fcccabbbf8
amirah <mirez.data@gmail.com>
2023-12-15 23:04:10 +0800

    commit

---
+++ src/components/motivData.js
@@ -0,0 +1,43 @@
+export default [ 
+    {
+        id: 1, 
+        content: "Finding a deeper meaning and aligning actions with values fuels a sense of fulfillment."
+    },
+    {
+        id: 2, 
+        content: "Embracing love without expectations nurtures relationships, fostering trust and growth within families and partnerships."
+    }, 
+    {
+        id: 3,
+        content: "Open, empathetic communication nurtures understanding and strengthens bonds within families and partnerships."
+
+    }, 
+    {
+        id: 4, 
+        content: "Embracing growth through learning and new experiences fosters personal and professional development."
+    }, 
+    {
+        id: 5, 
+        content: " Facing challenges with resilience and adaptability fosters personal growth and strengthens relationships, both in love and family life and in career success."    
+    }, 
+    {
+        id: 6, 
+        content: "Understanding others’ perspectives and showing kindness fosters stronger connections in all aspects of life."
+    }, 
+    {
+        id: 7, 
+        content: "Establishing clear, achievable goals provides direction and purpose, propelling one's career and personal life forward."
+    }, 
+    {
+        id: 8, 
+        content: "Striking a balance between career ambitions and personal life sustains mental well-being and enriches relationships."
+    }, 
+    {
+        id: 9, 
+        content: "Collaborating and supporting others cultivates a sense of belonging and amplifies success, both in family life and professional endeavors."
+    }, 
+    {
+        id: 10, 
+        content: "Practicing mindfulness and being present in the moment enhances self-awareness and relationships, enriching life and career experiences."
+    }
+];
