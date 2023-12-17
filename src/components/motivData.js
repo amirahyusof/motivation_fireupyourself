2f140e2f28b926dad37f0f5f2b492bbbe087b1fb
amirah <mirez.data@gmail.com>
2023-12-17 11:15:05 +0800

    commit

--- src/components/motivData.js
+++ src/components/motivData.js
@@ -26,7 +26,7 @@ export default [
     }, 
     {
         id: 7, 
-        content: "Establishing clear, achievable goals provides direction and purpose, propelling one's career and personal life forward."
+        content: "The best among you are those who have the best manners and character."
     }, 
     {
         id: 8, 
@@ -38,6 +38,48 @@ export default [
     }, 
     {
         id: 10, 
+        content:"Finding meaning, aligning actions, creating impact, living authentically, embracing passions."
+    }, 
+    {
+        id: 11, 
+        content: "A kind word is a form of charity."
+    },  
+    {
+        id: 12, 
         content: "Practicing mindfulness and being present in the moment enhances self-awareness and relationships, enriching life and career experiences."
+    },  
+    {
+        id: 13, 
+        content: "Success is not just about making money. It’s about making a difference"
+    },  
+    {
+        id: 14, 
+        content: "You are not the opinion of someone who doesn't know you"
+    },  
+    {
+        id: 15, 
+        content: "No matter what happens in life, be good to people. Being good to people is a wonderful legacy to leave behind."
+    },  
+    {
+        id: 16, 
+        content: "Fearless is getting back up and fighting for what you want over and over again... even though every time you've tried before, you've lost."
+    },  
+    {
+        id: 17, 
+        content: "In a world where you could say anything, be kind."
+    }, 
+    {
+        id: 18, 
+        content: "Do not lose hope, nor be sad."
+    }, 
+    {
+        id: 19, 
+        content: "The most beloved deeds to Allah are those performed consistently, even if they are few"
+    }, 
+    {
+        id: 20, 
+        content: "Verily, with hardship, there is relief."
     }
+    
+    
 ];
