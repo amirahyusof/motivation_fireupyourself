d7d04f5179b87fd372a3e0d8f993459aca687724
amirah <mirez.data@gmail.com>
2023-12-17 11:21:53 +0800

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import "./index.css"
+import React from "react";
 
 function App() {
   return (
     <div className="square-section">
       <div className="content-section">
-      <h3 className="intro-part">Hey, I need motivation</h3>
-      <h4 className="motiv-part">Motivation by me</h4>
+      <h3 className="intro-part">Hey, I need motivation!</h3>
+      <p className="motiv-part">
+      "Fearless is getting back up and fighting for what you want over 
+      and over again...even though every time you've tried before, you've lost"</p>
       <button>FireUp 🔥 </button>
       </div>
     </div>
